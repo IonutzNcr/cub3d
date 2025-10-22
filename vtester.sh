b15fc58d6aa141b4223c87eb0ba5d39ea219ab8b
Ionut Nicoara <inicoara@f2r7s19.clusters.42paris.fr>
2025-10-22 13:51:47 +0200

    blabla jd

--- vtester.sh
+++ vtester.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-# Fichier de sortie
-OUTPUT_FILE="test_output"
-
-# Vider le fichier de sortie s'il existe déjà
-> "$OUTPUT_FILE"
 
 # Lancer chaque fichier de map dans maps/invalid avec valgrind
 for map_file in maps/invalid/*; do
     echo ">>> Testing $map_file" 
 
-    valgrind --leak-check=full --show-leak-kinds=all --track-fds=yes ./cub3D "$map_file" 2>&1 
+    valgrind --leak-check=full --show-leak-kinds=all --track-fds=yes --track-origins=yes ./cub3D "$map_file"
     
     echo -e "---\n" 
 done
  
 echo -e "-------------------\n" 
 
+# Lancer chaque fichier de map dans maps/invalid avec valgrind
+for map_file in maps/valid/*; do
+    echo ">>> Testing $map_file" 
+
+    valgrind --leak-check=full --show-leak-kinds=all --track-fds=yes  --track-origins=yes ./cub3D "$map_file"
+    
+    echo -e "---\n" 
+done
