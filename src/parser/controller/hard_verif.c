a1fadf744afbbe04e51c3a0208733c032a54d6db
Ionut Nicoara <inicoara@f2r8s15.clusters.42paris.fr>
2025-11-06 06:38:54 +0100

    new line forced to be btw first part and the map ...

--- src/parser/controller/hard_verif.c
+++ src/parser/controller/hard_verif.c
@@ -6,7 +6,7 @@
 /*   By: inicoara <inicoara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/01 01:47:14 by inicoara          #+#    #+#             */
-/*   Updated: 2025/11/06 03:13:28 by inicoara         ###   ########.fr       */
+/*   Updated: 2025/11/06 06:37:25 by inicoara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,13 +44,19 @@ int	hard_verif(char *argv[])
 	int		fd;
 	t_task	liste[6];
 	char	*line;
+	size_t		is;
 
+	is = 0;
 	init_list(liste);
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 		return (perror("Error: "), 1);
 	while (wgnl(&line, fd))
 	{
+		if (checked_list(liste) && *line == '\0')
+			is++;
+		if (checked_list(liste) && is == 0 && set_sgt_error(BTW_LN))
+			return (free(line), empty_gnl(fd), free_singleton(), print_error());
 		if (*line == '\0' && free_one(line))
 			continue ;
 		if (checked_list(liste))
